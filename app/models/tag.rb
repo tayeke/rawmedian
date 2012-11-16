1e2065fd8db03a3ff2dd061bbc48f684a95dd5fa
Taylor j. Eke <tayloreke@gmail.com>
2012-11-15 16:53:29 -0800

    created tables

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ActiveRecord::Base
   attr_accessible :title
-  has_many_and_belongs_to :videos
+  has_and_belongs_to_many :videos
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,4 @@
 class Video < ActiveRecord::Base
   attr_accessible :description, :permalink, :title, :vimeo_id
-  has_many_and_belongs_to :tags
+  has_and_belongs_to_many :tags
 end
