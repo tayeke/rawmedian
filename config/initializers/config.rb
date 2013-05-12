895fdc38e08c2c87e17b751aa60db1c7a2d3bccc
Taylor j. Eke <tayloreke@gmail.com>
2013-05-11 19:37:59 -0700

    updated some config options

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,2 +1,2 @@
-# FB_ID = Options.find_by_key(:fb_id).value
-# TWEET_COPY = Options.find_by_key(:tweet_copy).value
+FB_ID = Options.find_by_key(:fb_id).value
+TWEET_COPY = Options.find_by_key(:tweet_copy).value
