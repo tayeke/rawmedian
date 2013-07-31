ec08d2e80ba4cb1f66e687b77825d22afe0e1e8d
Taylor j. Eke <tayloreke@gmail.com>
2013-07-31 01:28:52 -0700

    please precompile

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,3 +1,4 @@
+# these should eventually change to work the way I intended ?
 EMAIL_TO = Options.find_by_key(:email_address).value
 FB_ID = Options.find_by_key(:fb_id).value
 TWEET_COPY = Options.find_by_key(:tweet_copy).value
