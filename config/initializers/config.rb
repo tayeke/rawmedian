f03f9c088375db53a315d96f92ec2ada5ee777de
Taylor j. Eke <tayloreke@gmail.com>
2013-07-31 01:04:01 -0700

    made about more friendly

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,3 +1,3 @@
-EMAIL_TO = Options.find_by_key(:email_address).value
-FB_ID = Options.find_by_key(:fb_id).value
-TWEET_COPY = Options.find_by_key(:tweet_copy).value
+# EMAIL_TO = Options.find_by_key(:email_address).value
+# FB_ID = Options.find_by_key(:fb_id).value
+# TWEET_COPY = Options.find_by_key(:tweet_copy).value
